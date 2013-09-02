55311e60777bc1c073d511df8de747db5e759e49
patw <patw.hi@gmail.com>
2013-09-02 23:12:40 +0800

    Add the test View. Try to render.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
+var UserList = Backbone.View.extend({
+
+    el: $('.page'),
+    render: function () {
+        this.$el.html("Content will be placed here!");
+    }
+
+});
+
 var Router = Backbone.Router.extend({
     routes: {
         '': 'home'
     }
 });
 
+var userList = new UserList();
+
 var router = new Router();
 router.on('route:home', function () {
-    console.log("Loaded the home page.");
+    userList.render();
 });
 
 Backbone.history.start();
